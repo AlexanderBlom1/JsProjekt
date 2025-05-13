b594f3339899655cd4829a45b7f0bd5dfaebeb3e
Alexander Blom <154071565+AlexanderBlom1@users.noreply.github.com>
2025-05-13 10:22:52 +0200

    Update main.js

--- JsProjekt/main.js
+++ JsProjekt/main.js
@@ -1,5 +1,15 @@
+// Slumpar ett tal mellan 1-6
 function RandomNumber() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
 console.log(RandomNumber());
+
+
+function RandomNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+console.log(RandomNumber());
+
+
